bdb41c0c6f824721fd9eac923de1387a1dcf1c2e
kerry <kerry@obdesign.com.tw>
2017-07-20 10:31:49 +0800

    commit

---
+++ MVC_CSRF_AJAX/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using MVC_CSRF_AJAX.Filters;
+using MVC_CSRF_AJAX.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CSRF_AJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AccountEdit()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AjaxValidateAntiForgeryToken]
+        public ActionResult Edit(Account model)
+        {
+            JObject jo = new JObject();
+
+            if (!ModelState.IsValid)
+            {
+                jo.Add("success", false);
+                jo.Add("message", "欄位有空缺，請輸入完整。");
+                return Content(JsonConvert.SerializeObject(jo), "application/json");
+            }
+
+            jo.Add("success", true);
+            return Content(JsonConvert.SerializeObject(jo), "application/json");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AccountEdit(Account model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ MVC_CSRF_AJAX/Models/AccountViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CSRF_AJAX.Models
+{
+    public class Account
+    {
+        [Required]
+        [Display(Name = "帳號")]
+        public string account { get; set; }
+        [Required]
+        [Display(Name = "名稱")]
+        public string name { get; set; }
+    }
+}
